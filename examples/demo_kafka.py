8f53972a2b514e01763f510a1f99307fd54dd3f4
Pete Hope <petehope@gmail.com>
2023-01-04 12:22:33 +0000

    :memo: Updated example to demo new features

--- examples/demo_kafka.py
+++ examples/demo_kafka.py
@@ -4,14 +4,24 @@ Copyright (C) 2018-2022 Bryant Moscon - bmoscon@gmail.com
 Please see the LICENSE file for the terms and conditions
 associated with this software.
 '''
-from typing import Optional
 from cryptofeed import FeedHandler
-from cryptofeed.backends.kafka import BookKafka, TradeKafka
-from cryptofeed.defines import L2_BOOK, TRADES
+from cryptofeed.backends.kafka import BookKafka, CandlesKafka, FundingKafka, OpenInterestKafka, TradeKafka, TickerKafka, LiquidationsKafka
+from cryptofeed.defines import L2_BOOK, CANDLES, TRADES, TICKER, FUNDING, OPEN_INTEREST, LIQUIDATIONS
 from cryptofeed.exchanges import Coinbase
+from cryptofeed.exchanges.okx import OKX
 
 
 """
+This Kafka backend uses the following CustomizeMixin features:
+
+- topic and key accept template strings.
+
+The template strings can contain the name of any str element found in types.pyx and/or the keyword `channel`. 
+These keywords will be replaced at runtime with the data from the exchange. 
+The templates also preserve case. E.g. `SYMBOL` = `BTC-USD`, `symbol` = `btc-usd`  
+See docs/customize_mixin.md for more details
+
+---
 The AIOKafkaProducer accepts configuration options passed as kwargs to the Kafka callback(s)
 either as individual kwargs, an unpacked dictionary `**config_dict`, or both, as in the example below.
 The full list of configuration parameters can be found at
@@ -24,14 +34,6 @@ $ kafka-console-consumer --bootstrap-server 127.0.0.1:9092 --topic trades-COINBA
 """
 
 
-class CustomTradeKafka(TradeKafka):
-    def topic(self, data: dict) -> str:
-        return f"{self.key}-{data['exchange']}"
-
-    def partition_key(self, data: dict) -> Optional[bytes]:
-        return f"{data['symbol']}".encode('utf-8')
-
-
 def main():
     common_kafka_config = {
         'bootstrap_servers': '127.0.0.1:9092',
@@ -40,9 +42,27 @@ def main():
         'connections_max_idle_ms': 20000,
     }
     f = FeedHandler({'log':{'filename': 'feedhandler.log', 'level': 'INFO'}})
-    cbs = {TRADES: CustomTradeKafka(client_id='Coinbase Trades', **common_kafka_config), L2_BOOK: BookKafka(client_id='Coinbase Book', **common_kafka_config)}
+    coinbase_cbs = {
+        L2_BOOK: BookKafka(topic='EXCHANGE-Channel-Symbol', key='cryptofeed-symbol', client_id='Coinbase Book', **common_kafka_config),
+        TRADES: TradeKafka(topic='Exchange-CHANNEL-SYMBOL', key='cryptofeed-symbol-side', client_id='Coinbase Trades', **common_kafka_config),
+        TICKER: TickerKafka(client_id='Coinbase Ticker', **common_kafka_config)
+    }
 
-    f.add_feed(Coinbase(max_depth=10, channels=[TRADES, L2_BOOK], symbols=['BTC-USD'], callbacks=cbs))
+    okx_cbs = {
+        L2_BOOK: BookKafka(topic='EXCHANGE-Channel-symbol', client_id='OKX Book', **common_kafka_config),
+        TICKER: TickerKafka(topic='exchange-channel-SYMBOL', client_id='OKX Ticker', **common_kafka_config), 
+        LIQUIDATIONS: LiquidationsKafka(topic='Exchange-Channel-Side-Status', **common_kafka_config), 
+        FUNDING: FundingKafka(topic='exchange-channel-SYMBOL', **common_kafka_config), 
+        OPEN_INTEREST: OpenInterestKafka(topic='EXCHANGE-channel-symbol', **common_kafka_config), 
+        TRADES: TradeKafka(topic='exchange-channel', **common_kafka_config),
+        CANDLES: CandlesKafka(key='SYMBOL', **common_kafka_config),
+    }
+    
+    
+    
+
+    f.add_feed(Coinbase(max_depth=10, channels=[TRADES, L2_BOOK, TICKER], symbols=['BTC-USD', 'ETH-USD', 'SOL-USD'], callbacks=coinbase_cbs))
+    f.add_feed(OKX(checksum_validation=True, symbols=['BTC-USDT-PERP'], channels=[CANDLES, TRADES, TICKER, FUNDING, OPEN_INTEREST, LIQUIDATIONS, L2_BOOK], callbacks=okx_cbs))
 
     f.run()
 
